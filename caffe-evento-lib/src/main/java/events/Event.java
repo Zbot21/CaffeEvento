99f0d9c49f0838d5fd25490187b53c14cd313672
EppOch <ep.perrine@gmail.com>
2016-07-01 19:48:28 -0400

    Implemented Event

--- caffe-evento-lib/src/main/java/events/Event.java
+++ caffe-evento-lib/src/main/java/events/Event.java
@@ -1,31 +1,37 @@
 package events;
 
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by chris on 7/1/16.
  */
 public class Event {
     private String eventName;
+    private Map<String, String> eventDetails;
 
     public Event(){
         eventName = null;
+        eventDetails = new HashMap<String, String>();
     }
 
     public Event(String eventName){
+        this.eventName = eventName; 
+        eventDetails = new HashMap<String, String>();
     }
 
     public String getEventName(){
-        return null;
+        return this.eventName;
     }
 
     public void setEventName(String name){
+        this.eventName = name;
     }
 
     public void setEventField(String field, String value) {
-
+        eventDetails.put(field, value);
     }
     public String getEventField(String field) {
-        return null;
+        return eventDetails.get(field);
     }
 }
