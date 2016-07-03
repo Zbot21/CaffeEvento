331d3e2e03b2fce782263cafeb291bb45e591ae7
chris <chrisfwin@gmail.com>
2016-07-02 20:03:50 -0400

    Adding Event Source listener registration.

--- caffe-evento-lib/src/main/java/events/EventSource.java
+++ caffe-evento-lib/src/main/java/events/EventSource.java
@@ -1,8 +1,24 @@
 package events;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by chris on 7/1/16.
  */
-public interface EventSource {
-    void addListener(EventSink theEventSink);
+public abstract class EventSource {
+
+    private List<EventSink> eventSinks = new ArrayList<>();
+
+    public void addListener(EventSink theEventSink) {
+        eventSinks.add(theEventSink);
+    }
+
+    public void removeListener(EventSink theEventSink) {
+        eventSinks.remove(theEventSink);
+    }
+
+    protected void registerEvent(Event theEvent) {
+        eventSinks.forEach(eventSink -> eventSink.receiveEvent(theEvent));
+    }
 }
