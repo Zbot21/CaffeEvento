e5afa7b969d3fd1a037f813f58442c77d960f997
EppOch <ep.perrine@gmail.com>
2016-07-14 01:14:30 -0400

    Started putting together the clock service

--- caffe-evento-lib/src/main/java/impl/service/defaults/sensors/ClockService.java
+++ caffe-evento-lib/src/main/java/impl/service/defaults/sensors/ClockService.java
@@ -1,8 +1,16 @@
 package impl.service.defaults.sensors;
 
+import api.event_queue.EventQueueInterface;
+import impl.service.AbstractService;
+
 /**
  * Created by chris on 7/4/16.
  */
-public class ClockService {
-
+public class ClockService extends AbstractService{
+    public ClockService(EventQueueInterface eventQueueInterface)
+    {
+        super(eventQueueInterface);
+        //Todo: Add handlers to eventQueueInterface
+        //Todo: Add EventSources to the eventQueueInterface
+    }
 }
